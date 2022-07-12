41fb44ef3af3502c3a7cdc094819b4341496f22e
Fahreza Raharjo <fahrezaraharjo92@gmail.com>
2022-07-12 23:51:58 +0700

    Link to details

--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -2,6 +2,7 @@ import { Button } from '@mui/material'
 import React from 'react'
 import './SearchPage.css'
 import SearchResult from '../../components/SearchResult/SearchResult'
+import { Link } from 'react-router-dom'
 
 
 
@@ -27,26 +28,29 @@ function SearchPage() {
           More filters
         </Button>
       </div>
-      <SearchResult
-        img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU"
-        location="Private room in center of London"
-        title="Stay at this spacious Edwardian House"
-        description="1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine"
-        star={4.73}
-        price="£30 / night"
-        total="£117 total"
-      />
-
-      <SearchResult
-        img="https://www.expatkings.com/wp-content/uploads/2018/10/Airbnb-rental-tips.-Hostmaker-1-620x349.jpg"
-        location="Private room in center of London"
-        title="Independant luxury studio apartment"
-        description="2 guest · 3 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen"
-        star={4.3}
-        price="£40 / night"
-        total="£157 total"
-      />
-
+      <Link to="/Details" style={{ textDecoration: "none" }}>
+        <SearchResult
+          img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU"
+          location="Private room in center of London"
+          title="Stay at this spacious Edwardian House"
+          description="1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine"
+          star={4.73}
+          price="£30 / night"
+          total="£117 total"
+        />
+      </Link>
+      <Link to="/Details" style={{ textDecoration: "none" }}>
+        <SearchResult
+          img="https://www.expatkings.com/wp-content/uploads/2018/10/Airbnb-rental-tips.-Hostmaker-1-620x349.jpg"
+          location="Private room in center of London"
+          title="Independant luxury studio apartment"
+          description="2 guest · 3 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen"
+          star={4.3}
+          price="£40 / night"
+          total="£157 total"
+        />
+      </Link>
+      <Link to="/Details" style={{ textDecoration: "none" }}>
       <SearchResult
         img="https://www.smartertravel.com/uploads/2017/07/Untitled-design-8.jpg"
         location="Private room in center of London"
@@ -56,6 +60,8 @@ function SearchPage() {
         price="£35 / night"
         total="£207 total"
       />
+      </Link>
+      <Link to="/Details" style={{ textDecoration: "none" }}>
       <SearchResult
         img="https://cdn.bisnow.net/fit?height=489&type=jpeg&url=https%3A%2F%2Fs3.amazonaws.com%2Fcdn.bisnow.net%2Fcontent%2Fimages%2F2017%2F05%2F59151d0978bbf_https_press_atairbnb_com_app_uploads_2016_12_midtown_4.jpeg&width=717&sign=FeltIPi9cOWA36nVIeDvZxwgtiCZrpUyMRdvyZviTUI"
         location="Private room in center of London"
@@ -65,15 +71,8 @@ function SearchPage() {
         price="£55 / night"
         total="£320 total"
       />
-      <SearchResult
-        img="https://media.cntraveler.com/photos/5a8f258bd363c34048b35aac/master/w_2250,h_1500,c_limit/airbnb-plus-london.jpg"
-        location="Private room in center of London"
-        title="Spacious Peaceful Modern Bedroom"
-        description="3 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Free parking · Dry Cleaning"
-        star={5.0}
-        price="£60 / night"
-        total="£450 total"
-      />
+      </Link>
+      <Link to="/Details" style={{ textDecoration: "none" }}>
       <SearchResult
         img="https://static.trip101.com/paragraph_media/pictures/001/676/061/large/969ae4bb-efd1-4fb9-a4e3-5cb3316dd3c9.jpg?1562227937"
         location="Private room in center of London"
@@ -83,6 +82,8 @@ function SearchPage() {
         price="£65 / night"
         total="£480 total"
       />
+      </Link>
+      <Link to="/Details" style={{ textDecoration: "none" }}>
       <SearchResult
         img="https://image.insider.com/585029a0dd0895bc548b4b8b?width=750&format=jpeg&auto=webp"
         location="Private room in center of London"
@@ -92,6 +93,7 @@ function SearchPage() {
         price="£90 / night"
         total="£650 total"
       />
+      </Link>
     </div>
   )
 }
